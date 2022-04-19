6314e5e319f913e022a66a9c163be682c569af59
Kraine07 <aundre.bushay@gmail.com>
2022-04-18 22:21:09 -0500

    added modal, updated and consolidated css, color scheme

--- myScript.js
+++ myScript.js
@@ -1,3 +1,22 @@
+const modalButton = document.getElementById("modal_btn");
+const closeButton = document.getElementById("close_btn");
+const modal = document.getElementsByClassName("modal")[0];
+
+
+modalButton.onclick = function(){
+    modal.style.display = "block";
+}
+
+closeButton.onclick = function(){
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 window.oncontextmenu = function(){
     return false;
 }
